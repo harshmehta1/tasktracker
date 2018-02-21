79815a88f3db79f803ad8a726f5f7fd9e6d63231
harshmehta1 <harshsmehta1@gmail.com>
2018-02-20 20:27:30 -0500

    updated mix.exs

--- mix.exs
+++ mix.exs
@@ -41,6 +41,7 @@ defmodule Tasktracker.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"}
+      {:distillery, "~> 1.5", runtime: false}
     ]
   end
 
